dc04885160ea44889ee39bd5ec504961507e74ad
Nohe <nohe.hinniger.foray@gmail.com>
2021-12-14 20:17:16 +0100

    footer v1

--- components/footer-item.vue
+++ components/footer-item.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>Footer</div>
+  <footer> 
+    <div class="links">
+      <icon-link src="facebook.svg" alt="facebook icon" href="https://facebook.com" class="link"/>
+      <icon-link src="instagram.svg" alt="instagram icon" href="https://instagram.com" class="link"/>
+    </div>
+    <div class="authors">
+      ©
+      <a href="https://www.tomorrowtides.com/nano7.html" target="_blank">CARREZ Jérémie</a>
+      -
+      <a href="https://www.nohehf.com" target="_blank">HINNIGER-FORAY Nohé</a>
+      -
+      <a href="https://www.tomorrowtides.com/nano7.html" target="_blank">Hi7Haut</a>  
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -8,6 +21,34 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  a {
+    color: white;  
+  }
+  footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 50px;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    background-color: $dark-grey;
+    color: white;
+    text-align: center;
 
+    .links {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 80px;  
+    }
+
+    .link {
+
+      height: 30px;
+      width: 30px;
+
+    }
+  }
 </style>
---
+++ components/icon-link.vue
@@ -0,0 +1,27 @@
+<template>
+  <a :href="href" target="_blank">
+    <img :src="require(`~/assets/svg/${src}`)" :alt="alt" >
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+      src: {
+        type: String,
+        required: true,
+      },
+      alt: {
+        type: String,
+        required: true,
+      },
+      href: {
+        type: String,
+        required: true,
+      },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
